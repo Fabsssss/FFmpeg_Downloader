5108a4b7d88fab04a8f3ca5a9664352a0d4e35f9
Fabsssss <plegt@protonmail.com>
2023-05-01 16:14:15 +0200

    erste tests

---
+++ FFmpeg Downloader/FFmpeg Downloader/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace FFmpeg_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.btn_variable = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_command_generiren = new System.Windows.Forms.Button();
+            this.lbl_speicherpath = new System.Windows.Forms.Label();
+            this.btn_speicherort = new System.Windows.Forms.Button();
+            this.tb_link = new System.Windows.Forms.TextBox();
+            this.lbl_path = new System.Windows.Forms.Label();
+            this.btn_file = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_c);
+            this.groupBox1.Controls.Add(this.btn_variable);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(552, 179);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FFMPEG Installieren";
+            // 
+            // btn_c
+            // 
+            this.btn_c.BackColor = System.Drawing.Color.Red;
+            this.btn_c.Location = new System.Drawing.Point(307, 118);
+            this.btn_c.Name = "btn_c";
+            this.btn_c.Size = new System.Drawing.Size(119, 29);
+            this.btn_c.TabIndex = 10;
+            this.btn_c.Text = "Hinzufügen";
+            this.btn_c.UseVisualStyleBackColor = false;
+            this.btn_c.Click += new System.EventHandler(this.btn_c_Click);
+            // 
+            // btn_variable
+            // 
+            this.btn_variable.BackColor = System.Drawing.Color.Red;
+            this.btn_variable.Location = new System.Drawing.Point(307, 71);
+            this.btn_variable.Name = "btn_variable";
+            this.btn_variable.Size = new System.Drawing.Size(119, 29);
+            this.btn_variable.TabIndex = 9;
+            this.btn_variable.Text = "Hinzufügen";
+            this.btn_variable.UseVisualStyleBackColor = false;
+            this.btn_variable.Click += new System.EventHandler(this.btn_variable_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(226, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Hinzufügen von FFMPEG in den C Ordner";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(270, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Hinzufügen von FFMPEG zu dem Systemvariablen";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(508, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Für dieses Programm wird FFMPEG benötigt dafür würde das Programm dies gerne inst" +
+    "allieren ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_command_generiren);
+            this.groupBox2.Controls.Add(this.lbl_speicherpath);
+            this.groupBox2.Controls.Add(this.btn_speicherort);
+            this.groupBox2.Controls.Add(this.tb_link);
+            this.groupBox2.Controls.Add(this.lbl_path);
+            this.groupBox2.Controls.Add(this.btn_file);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(841, 310);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Download Link Erstellen";
+            // 
+            // btn_command_generiren
+            // 
+            this.btn_command_generiren.Location = new System.Drawing.Point(17, 212);
+            this.btn_command_generiren.Name = "btn_command_generiren";
+            this.btn_command_generiren.Size = new System.Drawing.Size(809, 84);
+            this.btn_command_generiren.TabIndex = 16;
+            this.btn_command_generiren.Text = "Downloaden";
+            this.btn_command_generiren.UseVisualStyleBackColor = true;
+            this.btn_command_generiren.Click += new System.EventHandler(this.btn_command_generiren_Click);
+            // 
+            // lbl_speicherpath
+            // 
+            this.lbl_speicherpath.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_speicherpath.Location = new System.Drawing.Point(213, 72);
+            this.lbl_speicherpath.Name = "lbl_speicherpath";
+            this.lbl_speicherpath.Size = new System.Drawing.Size(613, 21);
+            this.lbl_speicherpath.TabIndex = 15;
+            this.lbl_speicherpath.Text = "Path";
+            // 
+            // btn_speicherort
+            // 
+            this.btn_speicherort.Location = new System.Drawing.Point(17, 64);
+            this.btn_speicherort.Name = "btn_speicherort";
+            this.btn_speicherort.Size = new System.Drawing.Size(190, 31);
+            this.btn_speicherort.TabIndex = 14;
+            this.btn_speicherort.Text = "Speicherort aussuchen";
+            this.btn_speicherort.UseVisualStyleBackColor = true;
+            this.btn_speicherort.Click += new System.EventHandler(this.btn_speicherort_Click);
+            // 
+            // tb_link
+            // 
+            this.tb_link.Location = new System.Drawing.Point(160, 35);
+            this.tb_link.Name = "tb_link";
+            this.tb_link.Size = new System.Drawing.Size(670, 23);
+            this.tb_link.TabIndex = 13;
+            this.tb_link.TextChanged += new System.EventHandler(this.tb_link_TextChanged);
+            // 
+            // lbl_path
+            // 
+            this.lbl_path.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_path.Location = new System.Drawing.Point(375, 173);
+            this.lbl_path.Name = "lbl_path";
+            this.lbl_path.Size = new System.Drawing.Size(447, 21);
+            this.lbl_path.TabIndex = 12;
+            this.lbl_path.Text = "Path";
+            // 
+            // btn_file
+            // 
+            this.btn_file.Location = new System.Drawing.Point(250, 166);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(119, 28);
+            this.btn_file.TabIndex = 11;
+            this.btn_file.Text = "Datei aussuchen";
+            this.btn_file.UseVisualStyleBackColor = true;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(231, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Oder txt Datei mit mehrern Links angeben:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Bitte VOE Link angeben:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(870, 355);
+            this.tabControl1.TabIndex = 15;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(862, 327);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(881, 684);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 385);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btn_c;
+        private Button btn_variable;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private GroupBox groupBox2;
+        private TextBox tb_link;
+        private Label lbl_path;
+        private Button btn_file;
+        private Label label7;
+        private Label label2;
+        private Label lbl_speicherpath;
+        private Button btn_speicherort;
+        private Button btn_command_generiren;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ FFmpeg Downloader/FFmpeg Downloader/Form1.cs
@@ -0,0 +1,285 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+namespace FFmpeg_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string value = "";
+            value = Environment.GetEnvironmentVariable("PATH");
+            if (value.Contains("ffmpeg"))
+            {
+                btn_variable.BackColor = Color.Green;
+            }
+            if (Directory.Exists("C:\\ffmpeg"))
+            {
+                btn_c.BackColor = Color.Green;
+            }
+        }
+
+        public static string get_link(string url)
+        {
+            string befehl = " -i "+ '"'.ToString() + url + '"'.ToString()+" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 ";
+            return befehl;
+        }
+
+
+
+        public static string getSavedHtmlCode(string url)
+        {
+            string html = string.Empty;
+            try
+            {
+                var request = System.Net.HttpWebRequest.Create(string.Format("{0}", url));
+                request.Method = "GET";
+                var response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                html = readStream.ReadToEnd();
+                Console.WriteLine("Response stream received.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            string[] speicher;
+            try
+            {
+                speicher = (html.Split("const sources = {")[1]).Split("'");
+            }
+            catch
+            {
+                try
+                {
+                    speicher = (html.Split("var sources = {")[1]).Split("'");
+                }
+                catch
+                {
+                    MessageBox.Show("Fehler aufgetreten 1");
+                    return html;
+                }
+            }
+            foreach (string s in speicher)
+            {
+                if (s.Contains("m3u8"))
+                {
+                    return s;
+                }
+            }
+            MessageBox.Show("Fehler aufgetreten 2");
+            return html;
+        }
+
+
+        private void download_start(string argument)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.CreateNoWindow = false;
+            psi.UseShellExecute = false;
+            psi.FileName = "ffmpeg.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.Arguments = argument; 
+            using (Process ep = Process.Start(psi))
+            {
+                ep.WaitForExit();
+            }
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            //tb_generieter_link.Text = getfilename(tb_link.Text);
+        }
+
+        public static string getfilename(string url)
+        {
+            string html = string.Empty;
+            try
+            {
+                var request = System.Net.HttpWebRequest.Create(string.Format("{0}", url));
+                request.Method = "GET";
+                var response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                html = readStream.ReadToEnd();
+                Console.WriteLine("Response stream received.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            string[] speicher;
+            try
+            {
+                speicher = html.Split("<title>");
+                return speicher[1].Split("</title>")[0];
+            }
+            catch
+            {
+                MessageBox.Show("Fehler aufgetreten 3");
+                return html;
+            }
+            
+            return html;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        private void btn_variable_Click(object sender, EventArgs e)
+        {
+            string value = "";
+            value = Environment.GetEnvironmentVariable("PATH");
+            if (!value.Contains("ffmpeg"))
+            {
+                Environment.SetEnvironmentVariable("PATH", value + ";C:\\ffmpeg\\bin", EnvironmentVariableTarget.User);
+                value = Environment.GetEnvironmentVariable("PATH");
+                if (value.Contains("ffmpeg")) 
+                {
+                    btn_variable.BackColor = Color.Green;
+                }
+                else
+                {
+                    MessageBox.Show("Irgendwas scheint schief gelaufen zu sein");
+                }
+            }
+            else
+            {
+                btn_variable.BackColor = Color.Green;
+                MessageBox.Show("FFMPEG ist bereits installiert");
+            }
+        }
+
+        private void btn_c_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists("C:\\ffmpeg"))
+            {
+                DirectoryInfo t = new DirectoryInfo("C:\\ffmpeg");
+                DirectoryInfo s = new DirectoryInfo("ffmpeg");
+                CopyAll(s, t);
+                if (Directory.Exists("C:\\ffmpeg"))
+                {
+                    btn_c.BackColor = Color.Green;
+                }
+                else
+                {
+                    MessageBox.Show("Irgendwas scheint schief gelaufen zu sein");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("FFMEPG ist bereits im C Ordner Vorhanden");
+                btn_c.BackColor = Color.Green;
+            }
+            //ffmpeg -i "https://delivery-node-kaliq.voe-network.net/engine/hls2/01/00491/8rg59ghj3vwf_n/master.m3u8?t=bTh4ZcA0XkG4Nkk9KwOEvsKmD9_SWUgPsIImvHCr-0w&s=1668333357&e=14400&f=2455631&node=delivery-node-kaliq.voe-network.net&i=89.1&sp=4500&asn=8422" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 C:\\Users\\Fabian\\Desktop\\gg.mp4
+
+        }
+
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\Desktop\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tb_link.Text = "";
+                    lbl_path.Text = openFileDialog.FileName;
+                    lbl_speicherpath.Text = Path.GetDirectoryName(openFileDialog.FileName);
+                }
+            }
+            
+            //foreach (string line in System.IO.File.ReadLines(@"c:\test.txt"))
+            //{
+            //    System.Console.WriteLine(line);
+            //}
+        }
+
+
+
+        private void btn_speicherort_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    lbl_speicherpath.Text = (fbd.SelectedPath).ToString();
+                }
+            }
+        }
+
+        private void tb_link_TextChanged(object sender, EventArgs e)
+        {
+            lbl_path.Text = "Path";
+        }
+
+        private void btn_command_generiren_Click(object sender, EventArgs e)
+        {
+            string argument = "";
+            if((tb_link.Text == "" &&lbl_path.Text == "Path") || lbl_speicherpath.Text == "Path")
+            {
+                MessageBox.Show("Sie müssen alle Daten angeben");
+                return;
+            }
+            if (tb_link.Text == "")
+            {
+                foreach (string line in System.IO.File.ReadLines(lbl_path.Text))
+                {
+                    string getet_link = get_link(getSavedHtmlCode(line));
+                    if(!(getet_link.Contains('<') && getet_link.Contains('>')) && getet_link != "")
+                    {
+                        argument = get_link(getSavedHtmlCode(line)) + '"'.ToString() + lbl_speicherpath.Text + "\\" + getfilename(line).Replace("|", "") + ".mp4" + '"'.ToString();
+                        download_start(argument);
+                    }
+                }
+                
+            }
+            else
+            {
+                argument = get_link(getSavedHtmlCode(tb_link.Text)) + '"'.ToString() + lbl_speicherpath.Text + "\\" + getfilename(tb_link.Text).Replace("|","") + ".mp4" + '"'.ToString();
+                download_start(argument);
+            }
+          }
+
+
+    }
+}
